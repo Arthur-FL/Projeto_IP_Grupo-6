c304accf52f324e53ea7dc65275554b0c5e6b68d
gabriellri0 <griotintocavalcanti@gmail.com>
2025-03-27 10:47:01 -0300

    Add files via upload

--- enemy.py
+++ enemy.py
@@ -1,15 +1,16 @@
+
 import pygame
 from pygame.math import Vector2
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, waypoints):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.waypoints = waypoints
+        self.waypoints = [(0,360), (175, 360), (175, 160), (390, 160), (390, 440), (680, 440), (680, 300), (1121, 300)]
         self.pos = Vector2(self.waypoints[0])
         self.target_waypoint = 1
         self.speed = 1.5
-        self.image = pygame.image.load('Assets/inimigo1.png.webp').convert_alpha()
+        self.image = pygame.image.load('Assets/enemy1.webp').convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
 
@@ -33,3 +34,30 @@ class Enemy(pygame.sprite.Sprite):
 
         self.pos += self.movement.normalize() * self.speed
         self.rect.center = self.pos
+
+
+class enem_type1(Enemy):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.speed = 1
+        self.health = 100
+        self.image = pygame.image.load('Assets/inimigo1.png').convert_alpha()
+
+
+class enem_type2(Enemy):
+    def __init__(self):
+        super().__init__()
+        
+        self.speed = 5
+        self.health = 100
+        self.image = pygame.image.load('Assets/inimigo2.png').convert_alpha()
+
+class enem_type3(Enemy):
+    def __init__(self):
+        super().__init__()
+        
+        self.speed = 2
+        self.health = 100
+        self.image = pygame.image.load('Assets/inimigo3.png').convert_alpha()
