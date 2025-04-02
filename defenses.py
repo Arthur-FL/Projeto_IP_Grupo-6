7654e7352c60d1cbefe6a420dafdcca6c3de49af
gabriellri0 <griotintocavalcanti@gmail.com>
2025-04-01 23:37:21 -0300

    Add files via upload

--- defenses.py
+++ defenses.py
@@ -10,24 +10,25 @@ class defense(pygame.sprite.Sprite):
                 pygame.sprite.Sprite.__init__(self, self.groups)
 
 
-                self.image = pygame.transform.scale(pygame.image.load('Assets/enemy1.png'), (50,50))
+                self.image = pygame.transform.scale(pygame.image.load('Assets/enemy1.png'), (60,60))
                 self.pos = Vector2(pos)
                 self.rect = self.image.get_rect()
                 self.rect.center = self.pos
                 self.pos[1] += 25
 
                 #atributos diferentes nas torres
-                self.fire_rate = 10
-                self.dano = 15
-                self.range = 165
+                self.fire_rate = 1
+                self.dano = 1 # dano ou tipo de projetil
+                self.range = 1
+                self.cost = 0
                 #
                 
                 
-                self.cooldown = 100
+                self.cooldown = 1000
 
         def update(self):
                 
-                self.cooldown -= self.fire_rate 
+                
                 if self.cooldown <= 0:
                         for enemy in self.game.enemy_group:
                         
@@ -35,35 +36,84 @@ class defense(pygame.sprite.Sprite):
                                 dist = vector.length()
 
                                 if dist <= self.range:
-                                        enemy.vida -= self.dano
-                                        self.cooldown = 100
+                                        
+                                        self.shoot(enemy)
+                                        self.cooldown = 1000
                                         break
+                
+                else:
+                        self.cooldown -= self.fire_rate 
+
+        def shoot(self, enemy):
+                enemy.hp -= self.dano
 
 
-class def_type1(defense):
+class def_type1(defense): # basico
         def __init__(self, game, pos):
                 super().__init__(game, pos)
                 
+                self.image = pygame.transform.scale(pygame.image.load('Assets/enemy1.png'), (60,60))
                 
-                self.fire_rate = 10
-                self.dano = 15
-                self.range = 165
+                self.fire_rate = 18
+                self.dano = 30
+                self.range = 175
+                self.cost = 0
 
-class def_type2(defense):
+class def_type2(defense): # range alto, firerate baixo
         def __init__(self, game, pos):
                 super().__init__(game, pos)
 
-                self.fire_rate = 10
-                self.dano = 15
-                self.range = 220
+                self.image = pygame.transform.scale(pygame.image.load('Assets/enemy1.png'), (60,60))
 
-class def_type3(defense):
+                self.fire_rate =  12
+                self.dano = 75
+                self.range = 250
+                self.cost = 0
+
+class def_type3(defense): # range baixo, dps alto 
         def __init__(self, game, pos):
                 super().__init__(game, pos)
 
+                self.image = pygame.transform.scale(pygame.image.load('Assets/enemy1.png'), (60,60))
+
+                self.fire_rate = 100
+                self.dano = 25
+                self.range = 125
+                self.cost = 0
+
+
+class def_type4(defense): # empurra os inimigos :O
+        def __init__(self, game, pos):
+                super().__init__(game, pos)
+
+                self.image = pygame.transform.scale(pygame.image.load('Assets/enemy1.png'), (60,60))
+
+                self.fire_rate = 3
+                self.dano = 10
+                self.range = 160
+                self.cost = 0
+
+        def shoot(self,enemy):
+                enemy.target_waypoint = max(0, enemy.target_waypoint - 2)
+                enemy.hp -= self.dano
+
+
+   
+
+ 
+class def_type5(defense): # taca fogo nos inimigos sla
+        def __init__(self, game, pos):
+                super().__init__(game, pos)
+
+                self.image = pygame.transform.scale(pygame.image.load('Assets/enemy1.png'), (60,60))
+
                 self.fire_rate = 10
-                self.dano = 15
-                self.range = 100
+                self.dano = 0
+                self.range = 150
+                self.cost = 0
+
+        def shoot(self,enemy):
+                enemy.on_fire = True
 
                 
 
